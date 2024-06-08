f12acbaff87a3f99df068af45ce8ef8a347f56cd
Jeffrey Monzili <jeffrey.monzili@gmail.com>
2024-06-08 23:28:14 +0200

    Navbar + Banner

---
+++ kasa/src/components/Banner.jsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+import "./Banner.css"
+
+function Banner() {
+  return (
+    <div className="banner">
+      {/* <img src="https://unsplash.it/1240/223" alt="Bannière" /> */}
+      <h1>Chez vous, partout et ailleurs</h1>
+    </div>
+  )
+}
+
+export default Banner
