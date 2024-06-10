a6cd32361e87c6c1b22f018195781de7c5413321
Jeffrey Monzili <jeffrey.monzili@gmail.com>
2024-06-10 15:50:06 +0200

    Installation du Router

--- kasa/src/main.jsx
+++ kasa/src/main.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import App from './App.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/hello",
+    element: <h1>Oui mon Joky!!</h1>,
+  },
+
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
