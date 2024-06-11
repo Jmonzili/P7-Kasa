705d82dfb4907a3e45216ab871701ee88f02a2ec
Jeffrey Monzili <jeffrey.monzili@gmail.com>
2024-06-11 19:38:37 +0200

    SASS, layout, et router

--- kasa/src/pages/Home.jsx
+++ kasa/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 // import React from 'react'
-import ApartmentGrid from '../components/Home/ApartmentGrid'
-import Banner from '../components/Home/Banner'
+import ApartmentGrid from '../components/home/ApartmentGrid'
+import Banner from '../components/home/Banner'
 import './Home.scss'
 
 function Home() {
